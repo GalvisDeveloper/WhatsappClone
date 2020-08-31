61796e267dda822b6a8b822d0c2e370cecd049db
Cristian Bustos <cristianarbeybg@ufps.edu.co>
2020-08-31 16:26:49 -0500

    Starting App with Firebase features

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmwemQyJY3lgcmvBYa-awZ5LZcoJhk2vo",
+  authDomain: "whatsappclone-rjs.firebaseapp.com",
+  databaseURL: "https://whatsappclone-rjs.firebaseio.com",
+  projectId: "whatsappclone-rjs",
+  storageBucket: "whatsappclone-rjs.appspot.com",
+  messagingSenderId: "1033366575094",
+  appId: "1:1033366575094:web:f074c2bc1025db3f42d9d7",
+  measurementId: "G-1H78EKC6EC"
+};
